89d5c1d3e48b6efc38b10b1a464f82f12ac20b09
Ian Lyell <ian.lyell@yahoo.com>
2023-03-24 00:26:11 -0500

    Add comments and Boolean variables

--- variables.rb
+++ variables.rb
@@ -1,14 +1,20 @@
+# String variables
 email = "helloworld@gmail.com"
 name = "Ian Lyell"
 program = "Backend mod 0"
 date_of_birth = "05/16/1995"
-
+# Integer variables
 age = 28
 image_height = 200
 image_width = 300
 altitude = 13000
-
+# Float variables
 height_meters = 12.88
 bank_balance = 1234.56
 tempature_f = 86.65
 speed_mph = 543
+# Boolean variables
+is_present = true
+moving = false
+has_cat = true
+git_commited = true
